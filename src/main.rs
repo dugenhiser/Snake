3a3f30625381628188ac9790e2609f1bc69e67eb
dugenhiser <dougis.hogg@gmail.com>
2021-05-06 15:09:09 -0600

    First Window

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+
+fn main() {
+ App::build()
+ .add_startup_system(setup.system())
+ .add_plugins(DefaultPlugins)
+ .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
